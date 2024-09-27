5fed63516806e0365c27ab06a67da2f435d5347a
TakingCareofBusiness <superspongysquid@yahoo.com>
2024-09-27 12:12:14 -0400

    a lot

--- truthtablestuff.js
+++ truthtablestuff.js
@@ -1,18 +1,9 @@
 function animalAllowed(is_herbivore, weight, is_predator) {
-  if (is_herbivore === true && weight < 50) {
-    firstRule === true;
-  } else {
-    firstRule === false;
-  }
-  if (is_predator === true) {
-    secondRule === true;
-  } else {
-    secondRule === false;
-  }
-  if (firstRule === true && secondRule === true) {
-    console.log("The animal is allowed in the open area");
+  if ((is_herbivore === true || weight < 50) && !(is_predator === true)) {
+    if (is_predator === false)
+      console.log("The animal is allowed in the open area");
   } else {
     console.log("The animal is NOT allowed in the open area");
   }
 }
-animalAllowed(true, 30, true);
+animalAllowed(true, 100, false);
--- zoo.md
+++ zoo.md
@@ -1,2 +1,4 @@
-|Herbivore|Y|Z|
-|
+| Herbivore | Under 50 | Predator | Allowed |
+| --------- | -------- | -------- | ------- |
+| T         | T        | T        | F       |
+| T         | T        | F        | T       |
