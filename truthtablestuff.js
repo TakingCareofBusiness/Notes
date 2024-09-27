1bb596e563892e41932b2d87c506984392b13adc
TakingCareofBusiness <superspongysquid@yahoo.com>
2024-09-27 12:20:50 -0400

    jefferson

--- truthtablestuff.js
+++ truthtablestuff.js
@@ -1,9 +1,8 @@
 function animalAllowed(is_herbivore, weight, is_predator) {
-  if ((is_herbivore === true || weight < 50) && !(is_predator === true)) {
-    if (is_predator === false)
-      console.log("The animal is allowed in the open area");
+  if ((is_herbivore || weight < 50) && !is_predator) {
+    console.log("The animal is allowed in the open area");
   } else {
     console.log("The animal is NOT allowed in the open area");
   }
 }
-animalAllowed(true, 100, false);
+animalAllowed(true, 490, true);
