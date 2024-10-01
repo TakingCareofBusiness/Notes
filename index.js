b5a0af62482a7ddd0d0359c8afa60b19eb3e0230
TakingCareofBusiness <superspongysquid@yahoo.com>
2024-10-01 12:18:31 -0400

    what the fanum tax

--- index.js
+++ index.js
@@ -1,16 +1,2 @@
-function evenOrOdd(inputnumber) {
-  if (inputnumber % 2 > 0) {
-    console.log("odd number");
-  } else if (inputnumber % 2 === 0) {
-    console.log("even number");
-  }
-}
-function largerNumber(firstnumber, secondnumber) {
-  if (firstnumber > secondnumber) {
-    console.log(firstnumber, "is larger");
-  } else if (secondnumber > firstnumber) {
-    console.log(secondnumber, "is larger");
-  }
-}
-evenOrOdd(6);
-largerNumber(11, 5);
+const header = document.querySelector("h1");
+console.log(header);
